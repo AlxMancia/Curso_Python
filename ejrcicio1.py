f85e31b9a9c6fa93a3e45ff6d84d5f5ecb5ec45b
AlxMancia <alx.mancia@outlook.com>
2020-10-29 17:42:54 -0600

    Conceptos basicos

--- ejrcicio1.py
+++ ejrcicio1.py
@@ -1,3 +1,13 @@
-#Sirvio para probar el VS
+#manejo de variables
 msg = "Python modulo 1"
 print(msg)
+
+#importacion de librerias y uso de librerias
+import math
+print(math.e)
+print(math.pi)
+print(math.sin(2))
+numeros = [2,5,85,-81]
+print(len(numeros))
+print(max(numeros))
+print(min(numeros))
