fc2f390d947452a503c94f6ceec7076bbc6cd9fd
AlxMancia <alx.mancia@outlook.com>
2020-10-29 19:31:07 -0600

    Manejo de Variables y Operadores basicos

---
+++ variables2.py
@@ -0,0 +1,35 @@
+#Sesion 4 manejo de variables
+valor_uno = 100
+valor_dos = "Frank"
+valor_tres = 5*4
+print(valor_uno)
+print(valor_dos)
+print(valor_tres)
+#otra forma de asignar valores
+var1,var2,var3 = 44,"Yisus",45*8
+print(var1,var2,var3)
+
+#USO DE CONSTANTES
+CONSTANTE = "Esta es una constante"
+
+#OPERADORES RELACIONALES
+# mayor que >
+# menor que <
+# mayor o igual que >=
+# menor o igual que <=
+# comparacion ==
+# es diferente !=
+var1 = 5
+var2 = 20
+#retorna boolean
+mayor = var1 > var2
+print(mayor)
+
+#OPERADORES LOGICOS
+#AND, OR, NOT, IS
+resultado = True and True
+resultado2 =  True or False
+resultado3 = not True
+print(resultado,resultado2,resultado3)
+resultadoIS = var1 is 5
+print(resultadoIS)
