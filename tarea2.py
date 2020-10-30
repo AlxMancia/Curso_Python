f9dd484adcb76e4867560e4c36fccc1fda6f76a7
AlxMancia <alx.mancia@outlook.com>
2020-10-29 20:15:07 -0600

    Tarea 2

---
+++ tarea2.py
@@ -0,0 +1,17 @@
+#Ejercicio1
+base = float(input("Ingrese la base del triangulo:\n"))
+altura = float(input("Ingrese la altura del triangulo:\n"))
+print("El area del triangulo es:",(base*altura)/2,"u^2\n")
+
+#Ejercicio2
+cantDolares = float(input("Ingrese cantidad en dolares a convertir:\n"))
+print("Su cantidad en colones es:",cantDolares*8.75,"\n")
+
+#Ejercicio3
+cantColones = float(input("Ingrese cantidad en colones a convertir:\n"))
+print("Su cantidad en dolares es:",cantColones/8.75,"\n")
+
+#Ejercicio4
+distancia = float(input("Ingrese la distancia de su destino?(En Km)\n"))
+velocidad = float(input("Ingrese la velocidad a la que viaja?(En Km/m)\n"))
+print("Usted llegara a su destino en:",distancia/velocidad,"minutos")
